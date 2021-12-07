b95e7cd852d762c23ab7bbe3b31a957cd9630ef3
LeviThaicho <kalidasa@usc.edu>
2021-12-07 02:03:26 -0800

    commit on 'master'

--- top.sv
+++ top.sv
@@ -38,7 +38,7 @@ endgenerate
 
 wire [p-1:0:] lvt_select ;
 //------------------lvt instantiation-----------------//
-    LVT_register_based#(p,n_PE_bits,index_width)(
+    LVT_register_based#(p,n_PE_bits,index_width)lvt_inst(
         .clk(clk),
         .reset(reset),
         .addr(addr[p*index_width-1:0]),
@@ -47,18 +47,52 @@ wire [p-1:0:] lvt_select ;
         .lvt_sel(lvt_select)
     );
 
+//-----------------value registers----------------//
+integer ct=0;
+//wire [p * (p-1)-1:0][index_width-1:0] valid_reg_addr_wire;
+wire [p * ((p-1)/2)-1:0][n_r_bits-1:0] valid_reg_out_wire;
 
-//-----------------bram instantiation-------------/
-wire [value_width-1:0] value_wire [p*(p-1)-1:0][r-1:0];
+for (i = 0; i<p; i=i+1) begin
+    for (j=i+1; j<p; j=j+1)begin
+        valid_and_r_count_array #(
+            parameter index_width = 8,
+            parameter r = 4, 
+            parameter n_bits_r = 4
+        ) valid_inst(
+            .clk(clk), 
+            .reset(reset), 
+            .addr1({addr_wire[i][index_width-1]},{addr_wire[j][index_width-1]}),
+            .w_en({wen_wire[i]},{wen_wire[j]}),
+            .bram_index(valid_reg_out_wire[ct])
+    ); 
+    ct = ct+1;       
+    end
+end
+//-----------------bram instantiation-------------//
+
+wire [p*(p-1)-1:0][value_width-1:0] value_wire;
 
 generate
 integer ct=0;
-    for(i=0;i<p;i=i+1)row
-        for(j=i+1;j<p;j=j+1)column
-            for(k=0;k<r;k=k+1)replication begin
-                  
+    for(i=0;i<p;i=i+1)begin
+        for(j=i+1;j<p;j=j+1)begin          
+            for(k=0;k<r;k=k+1) begin
+                  blk_mem_gen_0 #(/*add parameters*/) uram_inst(
+                                    .clka(clk),
+                                    .ena(valid_reg_out_wire[ct][k]),
+                                    .wea(wen_wire[i]),
+                                    .addra(addr_wire[i]),
+                                    .dina(),   // write data from port wire
+                                    .douta(),  //connect data to wire, using select from lvt direct proper data to rread port
+                                    .clkb(clk),
+                                    .enb(valid_reg_out_wire[ct+1][k]),
+                                    .web(wen_wire[j]),
+                                    .addrb(addr_wire[j]),
+                                    .dinb(),     //
+                                    .doutb());     //           
             end   
-
+        end
+    end
 endgenerate
 
 
--- valid_array.v
+++ valid_array.v
@@ -1,12 +1,12 @@
 module valid_and_r_count_array #(
     parameter index_width = 8,
     parameter r = 4, 
-    parameter n_bits_r = 2
+    parameter n_bits_r = 4
 ) (
     input clk, 
     input reset, 
-    input [index_width-1:0]addr,
-    input [1:0]w_en;
+    input [2*index_width-1:0]addr1,
+    input [1:0]w_en,
     output [n_bits_r-1:0] bram_index
 );
 reg [n_bits_r-1:0] valid_array [2**index_width-1:0];
@@ -14,13 +14,13 @@ integer i;
 always @(posedge clk, negedge reset) begin
     if (~reset) begin
         for (i = 0; i<2**index_width-1; i=i+1) begin
-            valid_array[i] <= {n_bits_r{1'b0}};
+            valid_array[i] <= {n_bits_r-1{1'b0},{1'b1}};
         end
     end
     else begin
         for (i =0 ;i<2 ;i++ ) begin
           if (w_en[i]) begin
-            bram_index <= valid_array[addr];
+            bram_index <= valid_array[addr1[i*index_width+:index_width]];
             valid_array[addr] <= valid_array[addr] + 1;
          end
         end
